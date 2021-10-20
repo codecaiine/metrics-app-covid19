da96e0abaf4a21724cc49868a040df6353a340eb
codecaiine <akabrouyannickn@gmail.com>
2021-10-20 14:31:40 +0000

    Add Navbar and minimal style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/index.css';
 import App from './App';
 
 ReactDOM.render(

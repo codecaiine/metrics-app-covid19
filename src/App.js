0f3b31267a4efaca59dd23f6a559edc3860f1973
codecaiine <akabrouyannickn@gmail.com>
2021-10-20 14:22:16 +0000

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './assets/css/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My Covid App</h1>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/details">
+            <Details />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

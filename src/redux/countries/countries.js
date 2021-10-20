2b64d1e6b0b2d4bcdb3adfab62984e9e1b54b3e4
codecaiine <akabrouyannickn@gmail.com>
2021-10-20 17:34:22 +0000

    Add action creator

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -1,13 +1,13 @@
-import { getContinent } from './Api';
+import { getContinent, getCountry } from './Api';
 
 const LOAD_CONTINENT = 'covid-19-data-tracker/details/LOAD_CONTINENT';
+const LOAD_COUNTRY = 'covid-19-data-tracker/details/LOAD_COUNTRY';
 
 const initialState = [];
 
 export const loadContinent = () => async (dispatch) => {
   const continent = await getContinent();
   const countries = Object.values(continent);
-
   if (countries) {
     dispatch({
       type: LOAD_CONTINENT,
@@ -16,10 +16,24 @@ export const loadContinent = () => async (dispatch) => {
   }
 };
 
+export const loadCountry = (name) => async (dispatch) => {
+  const countryData = await getCountry(name);
+  const country = Object.values(countryData)[0];
+
+  if (country) {
+    dispatch({
+      type: LOAD_COUNTRY,
+      payload: country,
+    });
+  }
+};
+
 const countriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_CONTINENT:
       return action.payload;
+    case LOAD_COUNTRY:
+      return { ...action.payload };
     default:
       return state;
   }

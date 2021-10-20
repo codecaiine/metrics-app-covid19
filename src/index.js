343f7c66beaa6ef856259ff03166742c0718a032
codecaiine <akabrouyannickn@gmail.com>
2021-10-20 14:50:56 +0000

    Configure store and import it in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
